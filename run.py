4e82fa6059ef3cdf8e06e8f1a5a62399162de726
RozWelch <rozwelch@gmail.com>
2022-09-26 15:42:29 +0000

    Add text and style home page screen

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # code for accessing google spreadsheet taken from Love Sandwiches walk-through project
 import gspread
 from google.oauth2.service_account import Credentials
+from rich.text import Text
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,3 +21,17 @@ measurments = SHEET.worksheet('measurments')
 person_data = measurments.get_all_values()
 
 print(person_data)
+
+# print home screen page
+print(""" 
+......            ___  __       ___  __        __        ___  __   ......
+......  |\ | |  |  |  |__) |     |  |__)  /\  /  ` |__/ |__  |__)  ......
+......  | \| \__/  |  |  \ |     |  |  \ /~~\ \__, |  \ |___ |  \  ......
+ ......................................................................
+ .   o   \ o /  _ o        __|    \ /     |__         o _  \ o /   o   .
+ .  /|\    |     /\   __\o   \o    |    o/     o/__   /\     |    /|\  .
+ .  / \   / \   | \  /) |    ( \  /o\  / )    |   (\  / |   / \   / \  .
+ .......................................................................
+                                                          """)
+
+returning = input('Hello, have you used Nutri Tracker before? Yes/No ')
